ee63befddc30d1aaa80e24ff42d28bd21be59280
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2024-06-23 02:32:24 +0300

    drf_spectacular

---
+++ poetry-sagaart/api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
+
+urlpatterns = [
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path(
+        'schema/swagger-ui/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
+    path(
+        'schema/redoc/',
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc'
+    ),
+]
