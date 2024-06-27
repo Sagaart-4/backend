838976c52d15f5bc097629f023e3fe9aa91f92d4
Владислав Суворов <gordik_08@mail.ru>
2024-06-27 17:13:10 +0300

    feat: add field names conversion

--- poetry-sagaart/users/api/serializers.py
+++ poetry-sagaart/users/api/serializers.py
@@ -1,5 +1,7 @@
 """Модуль с сериализаторами для приложения users."""
 
+from api.serializers import CategorySerializer, StyleSerializer
+from artshop.models import Category, Style, StyleBuyerProfile
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from users.models import BuyerProfile, CustomUser, SellerProfile
@@ -21,23 +23,69 @@ class CustomUserCreateSerializer(UserCreateSerializer):
 
 
 class BuyerProfileSerializer(serializers.ModelSerializer):
-    """Получение списка или одного покупателя."""
+    """Сериализатор для модели профиля покупателя."""
+
+    lastName = serializers.CharField(source="surname")
+    surname = serializers.CharField(source="patronymic_surname")
+    phone = serializers.CharField(source="phone_number")
+    preferStyle = serializers.CharField(source="favorite_style")
+    preferCategory = serializers.CharField(source="favorite_category")
+    preferStyles = StyleSerializer(many=True, source="favorite_styles")
+    preferCategories = CategorySerializer(
+        many=True, source="favorite_categories"
+    )
 
     class Meta:
         """Класс с метаданными для сериализатора покупателя."""
 
         model = BuyerProfile
-        exclude = ("user",)
+        fields = (
+            "name",
+            "lastName",
+            "surname",
+            "phone",
+            "preferStyle",
+            "preferCategory",
+            "preferStyles",
+            "preferCategories",
+            "photo",
+        )
+
+        def update(self, instance, validated_data):
+            """Метод для обновления экземпляра модели профиля покупателя."""
+            styles = validated_data.pop("preferStyle")
+            categories = validated_data.pop("preferCategory")
+            buyer_profile_instance = SellerProfile.objects.get(
+                user=instance.id
+            )
+            for style in styles:
+                current_style, status = Style.objects.get_or_create(**style)
+                StyleBuyerProfile.objects.create(
+                    style=current_style, buyer_profile=buyer_profile_instance
+                )
+            for category in categories:
+                current_category, status = Category.objects.get_or_create(
+                    **category
+                )
+                StyleBuyerProfile.objects.create(
+                    category=current_category,
+                    buyer_profile=buyer_profile_instance,
+                )
+            return instance
 
 
 class SellerProfileSerializer(serializers.ModelSerializer):
-    """Получение списка или одного продавца."""
+    """Сериализатор для модели профиля продавца."""
+
+    lastName = serializers.CharField(source="surname")
+    surname = serializers.CharField(source="patronymic_surname")
+    phone = serializers.CharField(source="phone_number")
 
     class Meta:
         """Класс с метаданными для сериализатора продавца."""
 
         model = SellerProfile
-        exclude = ("user",)
+        fields = ("name", "lastName", "surname", "phone", "photo")
 
 
 class CustomUserIsBuyerSerializer(UserSerializer):
