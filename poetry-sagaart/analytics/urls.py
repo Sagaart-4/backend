9378b4fdd59c2275862cdb9a7b4d8a8396b9b736
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2024-07-03 14:02:59 +0300

    Seller price gen

--- poetry-sagaart/analytics/urls.py
+++ poetry-sagaart/analytics/urls.py
@@ -3,5 +3,8 @@ from .views import DataView
 # app_name = 'analytics'
 
 urlpatterns = [
-    path('user/analytics/', DataView.as_view({'post': 'post'}), name='analytics'),
+    path('user/analytics/', DataView.as_view({'post': 'post'}),
+         name='analytics'),
+    path('seller/analytics/', DataView.as_view({'post': 'post'}),
+         name='analytics'),
 ]
